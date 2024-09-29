0f0cdfb2657bdf36073e3190a8575ff4998ec650
Sonagara-Dhirajlal <sonagradhiraj574@gmail.com>
2024-09-29 18:12:43 +0530

    all folder added

---
+++ includes/script.php
@@ -0,0 +1,11 @@
+    <!-- Js Plugins -->
+    <script src="<?= urlOf('assets/js/jquery-3.3.1.min.js')?>"></script>
+    <script src="<?= urlOf('assets/js/bootstrap.min.js')?>"></script>
+    <script src="<?= urlOf('assets/js/jquery.nice-select.min.js')?>"></script>
+    <script src="<?= urlOf('assets/js/jquery.nicescroll.min.js')?>"></script>
+    <script src="<?= urlOf('assets/js/jquery.magnific-popup.min.js')?>"></script>
+    <script src="<?= urlOf('assets/js/jquery.countdown.min.js')?>"></script>
+    <script src="<?= urlOf('assets/js/jquery.slicknav.js')?>"></script>
+    <script src="<?= urlOf('assets/js/mixitup.min.js')?>"></script>
+    <script src="<?= urlOf('assets/js/owl.carousel.min.js')?>"></script>
+    <script src="<?= urlOf('assets/js/main.js')?>"></script>
